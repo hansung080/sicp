4cda667e569eb372e756d2f57f88675cfc38384d
hansung <hansung080@hanmail.net>
2026-08-19 16:00:36 +0900

    modified some trivial things in sum

--- scm/ch01/sum.scm
+++ scm/ch01/sum.scm
@@ -1,30 +1,35 @@
 (load "../common/math.scm")
 
-;; Higher-Order Procedure: Sum (Linear Recursive Process)
+;; Higher-Order Sum (Linear Recursive Process)
 ;;
-;;   sum-terms(a, b) = term(a) + term(next(a)) + term(next(a)) + ... + term(b)
-(define (sum term a next b)
+;;   sum(term, a, next, b) = term(a) + term(next(a)) + term(next(next(a))) + ... + term(b)
+;;
+(define (sum1 term a next b)
   (if (> a b)
       0
       (+ (term a)
-         (sum term (next a) next b))))
+         (sum1 term (next a) next b))))
 
-
-(define (sum-integers a b)
-  (if (> a b)
-      0
-      (+ a (sum-integers (+ a 1) b))))
 
 (define (sum-integers0 a b)
-  (sum identity a inc b))
-
-(define (sum-cubes a b)
   (if (> a b)
       0
-      (+ (cube a) (sum-cubes (+ a 1) b))))
+      (+ a (sum-integers0 (+ a 1) b))))
+
+(define (sum-integers1 a b)
+  (sum1 identity a inc b))
+
+(define sum-integers sum-integers0)
 
 (define (sum-cubes0 a b)
-  (sum cube a inc b))
+  (if (> a b)
+      0
+      (+ (cube a) (sum-cubes0 (+ a 1) b))))
+
+(define (sum-cubes1 a b)
+  (sum1 cube a inc b))
+
+(define sum-cubes sum-cubes0)
 
 ;; Leibniz Series for π/4
 ;;
@@ -34,19 +39,21 @@
 ;;
 ;;   1/(1*3) + 1/(5*7) + 1/(9*11) + ... = π/8
 ;;
-(define (pi-sum a b)
+(define (pi-sum0 a b)
   (if (> a b)
       0
-      (+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
+      (+ (/ 1.0 (* a (+ a 2))) (pi-sum0 (+ a 4) b))))
 
-(define (pi-sum0 a b)
+(define (pi-sum1 a b)
   (define (term x)
     (/ 1.0 (* x (+ x 2))))
   (define (next x)
     (+ x 4))
-  (sum term a next b))
+  (sum1 term a next b))
 
-;; Integral Approximation Using Riemann Sum
+(define pi-sum pi-sum0)
+
+;; Riemann Integral Approximation
 ;;
 ;;   Assumption
 ;;
@@ -54,18 +61,20 @@
 ;;
 ;;   Left Riemann Sum
 ;;
-;;     S<a, b>f(x)dx ≈ {f(a) + f(a + dx) + f(a + 2dx) + ... + f(b - dx)} * dx
+;;     ∫<a, b>f(x)dx ≈ {f(a) + f(a + dx) + f(a + 2dx) + ... + f(b - dx)} * dx
 ;;
 ;;   Midpoint Riemann Sum (implemented by integral)
 ;;
-;;     S<a, b>f(x)dx ≈ {f(a + dx/2) + f(a + dx/2 + dx) + f(a + dx/2 + 2dx) + ... + f(b - dx/2)} * dx
+;;     ∫<a, b>f(x)dx ≈ {f(a + dx/2) + f(a + dx/2 + dx) + f(a + dx/2 + 2dx) + ... + f(b - dx/2)} * dx
 ;;
 ;;   Right Riemann Sum
 ;;
-;;     S<a, b>f(x)dx ≈ {f(a + dx) + f(a + 2dx) + f(a + 3dx) + ... + f(b)} * dx
+;;     ∫<a, b>f(x)dx ≈ {f(a + dx) + f(a + 2dx) + f(a + 3dx) + ... + f(b)} * dx
 ;;
-(define (integral f a b dx)
+(define (integral1 f a b dx)
   (define (add-dx x)
     (+ x dx))
-  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+  (* (sum1 f (+ a (/ dx 2.0)) add-dx b)
      dx))
+
+(define integral integral1)
