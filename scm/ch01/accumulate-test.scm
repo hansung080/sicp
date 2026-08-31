3e19938eec5b401324c7b470d8d3a74725a1c279
hansung <hansung080@hanmail.net>
2026-08-31 19:56:17 +0900

    added filtered-accumulate

--- scm/ch01/accumulate-test.scm
+++ scm/ch01/accumulate-test.scm
@@ -1,6 +1,8 @@
 (load "../common/math.scm")
 (load "../common/testing.scm")
 (load "accumulate.scm")
+(load "gcd.scm")
+(load "prime.scm")
 
 (test "accumulate1"
       (lambda ()
@@ -8,12 +10,12 @@
           (accumulate1 + 0 term a next b))
         (define (sum-integers a b)
           (sum identity a inc b))
-        (assert-eq (sum-integers 1 4) 10)
-
         (define (product term a next b)
           (accumulate1 * 1 term a next b))
         (define (product-integers a b)
           (product identity a inc b))
+
+        (assert-eq (sum-integers 1 4) 10)
         (assert-eq (product-integers 1 4) 24)))
 
 (test "accumulate2"
@@ -22,10 +24,40 @@
           (accumulate2 + 0 term a next b))
         (define (sum-integers a b)
           (sum identity a inc b))
-        (assert-eq (sum-integers 1 4) 10)
-
         (define (product term a next b)
           (accumulate2 * 1 term a next b))
         (define (product-integers a b)
           (product identity a inc b))
+
+        (assert-eq (sum-integers 1 4) 10)
         (assert-eq (product-integers 1 4) 24)))
+
+(test "filtered-accumulate1"
+      (lambda ()
+        (define (sum-evens a b)
+          (filtered-accumulate1 even?_ + 0 identity a inc b))
+        (define (sum-prime-squares a b)
+          (filtered-accumulate1 prime? + 0 square a inc b))
+        (define (product-coprimes n)
+          (define (coprime? i)
+            (= (gcd_ i n) 1))
+          (filtered-accumulate1 coprime? * 1 identity 1 inc (- n 1)))
+
+        (assert-eq (sum-evens 1 10) 30)
+        (assert-eq (sum-prime-squares 1 10) 87)
+        (assert-eq (product-coprimes 10) 189)))
+
+(test "filtered-accumulate2"
+      (lambda ()
+        (define (sum-evens a b)
+          (filtered-accumulate2 even?_ + 0 identity a inc b))
+        (define (sum-prime-squares a b)
+          (filtered-accumulate2 prime? + 0 square a inc b))
+        (define (product-coprimes n)
+          (define (coprime? i)
+            (= (gcd_ i n) 1))
+          (filtered-accumulate2 coprime? * 1 identity 1 inc (- n 1)))
+
+        (assert-eq (sum-evens 1 10) 30)
+        (assert-eq (sum-prime-squares 1 10) 87)
+        (assert-eq (product-coprimes 10) 189)))
--- scm/ch01/accumulate.scm
+++ scm/ch01/accumulate.scm
@@ -20,3 +20,27 @@
   (iter null-value a))
 
 (define accumulate accumulate2)
+
+;; Higher-Order Filtered-Accumulate
+;;
+;;   filtered-accumulate(predicate?, combiner, null-value, term, a, next, b)
+;;   = combiner(term(a), term(next(a)), term(next(next(a))), ..., term(b))
+;;     for values a' where predicate?(a') is true
+;;
+;; Linear Recursive Process
+(define (filtered-accumulate1 predicate? combiner null-value term a next b)
+  (cond ((> a b) null-value)
+        ((predicate? a) (combiner (term a)
+                                  (filtered-accumulate1 predicate? combiner null-value term (next a) next b)))
+        (else (filtered-accumulate1 predicate? combiner null-value term (next a) next b))))
+
+;; Linear Iterative Process
+(define (filtered-accumulate2 predicate? combiner null-value term a next b)
+  (define (iter result a)
+    (cond ((> a b) result)
+          ((predicate? a) (iter (combiner result (term a))
+                                (next a)))
+          (else (iter result (next a)))))
+  (iter null-value a))
+
+(define filtered-accumulate filtered-accumulate2)
