22972ed1565e55a12b1e9f51a9678c5b6c2b0213
hansung <hansung080@hanmail.net>
2026-09-10 17:48:09 +0900

    added a cubic-root test

--- scm/ch01/polynomial-roots-test.scm
+++ scm/ch01/polynomial-roots-test.scm
@@ -16,4 +16,6 @@
 (test "cubic-root"
       (lambda ()
         ;; x^3 - 2x - 3 = 0 -> x_1 = cbrt{3/2 + sqrt(211/108)} + cbrt{3/2 - sqrt(211/108)} ≈ 1.8932891963
-        (assert-eq (cubic-root 1 0 -2 -3) 1.8932891963044978)))
+        (assert-eq (cubic-root 1 0 -2 -3) 1.8932891963044978)
+        ;; 2x^3 + 5x^2 + 5x + 3 = 0 -> (2x + 3)(x^2 + x + 1) = 0 -> x_1 = -3/2
+        (assert-eq (cubic-root 2 5 5 3) -1.4999999999999998)))
